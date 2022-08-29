0b23ebf16b202a24e6583e2eb99c3861219ef047
AndyHeaven <82590935@qq.com>
2022-08-29 17:10:24 +0800

    first commit

---
+++ src/main/java/com/zhy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhy.git;
+
+/**
+ * @Author zhy
+ * @Date 2022/8/29 17:07
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
